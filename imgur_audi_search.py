ca7c5f07476046d8aca153ddd3e0cf9f90509b82
Piotr Kowalski <pkowalski7@sii.pl>
2023-02-13 11:44:58 +0100

    updated

--- imgur_audi_search.py
+++ imgur_audi_search.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-#from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
